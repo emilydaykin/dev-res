bafa63cedb0348ac90e1c5eb4972f30fea2f3500
Emily Daykin <emilydaykin31@gmail.com>
2023-04-29 19:01:54 +0200

    broke down DevResources into another component to make App.vue leaner

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
 <template>
   <div class="border">
     <h1>Hello World</h1>
-    <ul>
-      <dev-resources 
-        v-for="resource in storedResources" 
-        v-bind:key="resource.id" 
-        :title="resource.title" 
-        :description="resource.description"
-        :link="resource.link"
-      ></dev-resources>
-    </ul>
+    <stored-resources :resources="storedResources"></stored-resources>
   </div>
-  
 </template>
 
 <script>
-import DevResources from './components/dev-resources/DevResources.vue'
+  import StoredResources from './components/dev-resources/StoredResources.vue';
 
-export default {
-  name: 'App',
-  components: {
-    DevResources
-  },
-  data() {
-    return {
-      storedResources: [
-        {id: 1, title: 'VueJS', description: 'The official guide to Vue.js', link: 'https://vuejs.org'},
-        {id: 2, title: 'Tailwind', description: 'The official guide to Tailwind CSS', link: 'https://tailwindcss.com/'},
-        {id: 3, title: 'Nuxt', description: 'The official guide to Nuxt.js', link: 'https://nuxtjs.org/'},
-        {id: 4, title: 'ReactJS', description: 'The official guide to ReactJS', link: 'https://react.dev/'},
-        {id: 5, title: 'Git', description: 'The official guide to Git', link: 'https://git-scm.com/'},
-      ]
-    }
-  }
-
-}
+  export default {
+    name: 'App',
+    components: {
+      StoredResources,
+    },
+    data() {
+      return {
+        storedResources: [
+          {
+            id: 1,
+            title: 'VueJS',
+            description: 'The official guide to Vue.js',
+            link: 'https://vuejs.org',
+          },
+          {
+            id: 2,
+            title: 'Tailwind',
+            description: 'The official guide to Tailwind CSS',
+            link: 'https://tailwindcss.com/',
+          },
+          {
+            id: 3,
+            title: 'Nuxt',
+            description: 'The official guide to Nuxt.js',
+            link: 'https://nuxtjs.org/',
+          },
+          {
+            id: 4,
+            title: 'ReactJS',
+            description: 'The official guide to ReactJS',
+            link: 'https://react.dev/',
+          },
+          {
+            id: 5,
+            title: 'Git',
+            description: 'The official guide to Git',
+            link: 'https://git-scm.com/',
+          },
+        ],
+      };
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 </style>
---
+++ src/components/dev-resources/StoredResources.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <dev-resources
+      v-for="resource in resources"
+      v-bind:key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></dev-resources>
+  </ul>
+</template>
+
+<script>
+  import DevResources from './DevResources.vue';
+
+  export default {
+    props: ['resources'],
+    components: {
+      DevResources,
+    },
+  };
+</script>
