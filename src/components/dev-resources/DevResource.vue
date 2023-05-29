9f7c67a441077864da7c089c53650f72e10b76fa
Emily Daykin <emily.daykin@blinkist.com>
2023-05-29 17:58:52 +0200

    renamed DevResources to DevResource

---
+++ src/components/dev-resources/DevResource.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <base-card class="flex flex-col sm:flex-row sm:justify-between">
+      <div class="border-6 border-black">
+        <header>
+          <h3 class="font-bold mb-2">{{ title }}</h3>
+        </header>
+        <p>{{ description }}</p>
+        <nav class="mt-2">
+          <a :href="link" target="_blank" class="text-sky-300 font-bold italic">View link</a>
+        </nav>
+      </div>
+      <base-button additionalStyling="border-red-100 text-red-500 hover:bg-red-100 mt-3 sm:mt-0 self-end sm:self-start" @click="deleteResource(id)">Delete</base-button>
+    </base-card>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['id', 'title', 'description', 'link'],
+    inject: ['deleteResource']
+  };
+</script>
--- src/components/dev-resources/StoredResources.vue
+++ src/components/dev-resources/StoredResources.vue
@@ -1,23 +1,23 @@
 <template>
   <ul class="w-9/12 mx-auto max-w-3xl">
-    <dev-resources
+    <dev-resource
       v-for="resource in resources"
       v-bind:key="resource.id"
       :id="resource.id"
       :title="resource.title"
       :description="resource.description"
       :link="resource.link"
-    ></dev-resources>
+    ></dev-resource>
   </ul>
 </template>
 
 <script>
-  import DevResources from './DevResources.vue';
+  import DevResource from './DevResource.vue';
 
   export default {
     inject: ['resources'], // inject them instead of passing them as props
     components: {
-      DevResources,
+      DevResource,
     },
   };
 </script>
