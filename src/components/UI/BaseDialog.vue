cfed81b5a143e5f2085a6e407e690ab8e03b1fda
Emily Daykin <emily.daykin@blinkist.com>
2023-05-29 17:06:56 +0200

    teleported dialog modal 'to body' to better structure the DOM

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,21 +1,24 @@
 <!-- used for showing error messages -->
 <template>
-  <div class="bg-zinc-800/[.8] h-full w-full fixed top-0" @click="$emit('close')"></div>
-  <dialog open class="border-2 rounded-lg p-16 fixed">
-    <header>
-      <slot name="header">
-        <h2 class="text-lg uppercase text-red-600 font-bold text-center mb-4">{{ title }}</h2>
-      </slot>
-    </header>
-    <section>
-      <slot></slot>
-    </section>
-    <menu class='flex justify-center mt-6'>
-      <slot name="actions">
-        <base-button @click="$emit('close')" class="border-2 border-slate-400 hover:bg-slate-300">Close</base-button>
-      </slot>
-    </menu>
-  </dialog>
+  <!-- "teleport to body" brings this to the end of the body (so that it's not randomly jumbled in the DOM) -->
+  <teleport to="body">
+    <div class="bg-zinc-800/[.8] h-full w-full fixed top-0" @click="$emit('close')"></div>
+    <dialog open class="border-2 rounded-lg p-16 fixed top-1/4 font-sans">
+      <header>
+        <slot name="header">
+          <h2 class="text-lg uppercase text-red-600 font-bold text-center mb-4">{{ title }}</h2>
+        </slot>
+      </header>
+      <section>
+        <slot></slot>
+      </section>
+      <menu class='flex justify-center mt-6'>
+        <slot name="actions">
+          <base-button @click="$emit('close')" class="border-2 border-slate-400 hover:bg-slate-300">Close</base-button>
+        </slot>
+      </menu>
+    </dialog>
+  </teleport>
 </template>
 
 <script>
@@ -29,3 +32,9 @@ export default {
   emits: ['close']
 }
 </script>
+
+<style>
+  dialog > * {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+</style>
